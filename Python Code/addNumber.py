32c7a80ee20a939b22704e25ace7635fffa83f57
saurabhjha137 <saurabhjha137@gmail.com>
2023-05-20 18:06:12 +0530

    added test for new delimeter

--- Python Code/addNumber.py
+++ Python Code/addNumber.py
@@ -1,13 +1,20 @@
+import re
 
 def add(numbers):
     if not numbers:
         return 0
     
     delimiter = ","
-    number = numbers.split(",")
+    
+    if numbers.startswith("//"):
+        delimiter = numbers[2]
+        numbers = numbers[numbers.index("\n")+1:]
+       
+    numbers = re.split(f"[{delimiter}\n]", numbers)
+    
 
-    return sum(int(num) for num in number if int(num) <= 1000)
+    return sum(int(num) for num in numbers if int(num) <= 1000)
 
 
-print(add("1"))
-print(add("1,2,3,4"))
+print(add("//;\n1;2"))
+print(add("1\n2"))
